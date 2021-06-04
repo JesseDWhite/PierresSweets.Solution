48d3270722215600b7d4685a9a8d00c1083ae4c9
Jesse White <jesse.white6@gmail.com>
2021-06-04 08:39:23 -0700

    add context and designtime markup.

---
+++ PierresSweets/Models/PierresSweetsContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace PierresSweets.Models
+{
+  public class PierresSweetsContext : IdentityDbContext<ApplicationUser>
+  {
+    public virtual DbSet<Flavor> Flavors { get; set; }
+    public DbSet<Treat> Treats { get; set; }
+    public DbSet<FlavorTreat> FlavorTreat { get; set; }
+
+    public PierresSweetsContext(DbContextOptions options) : base(options) { }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseLazyLoadingProxies();
+    }
+  }
+}
