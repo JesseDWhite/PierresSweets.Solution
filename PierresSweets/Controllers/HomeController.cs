d0248ace2ed6397c3170685abde5c9680dd249b5
Jesse White <jesse.white6@gmail.com>
2021-06-04 16:38:31 -0700

    add splash page that shows all treats and flavors.

--- PierresSweets/Controllers/HomeController.cs
+++ PierresSweets/Controllers/HomeController.cs
@@ -1,4 +1,13 @@
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc;
+using PierresSweets.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace PierresSweets.Controllers
 {
@@ -14,8 +23,12 @@ namespace PierresSweets.Controllers
     [HttpGet("/")]
     public ActionResult Index()
     {
-      return View();
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      List<Treat> treatList = _db.Treats.ToList();
+      List<Flavor> flavorList = _db.Flavors.ToList();
+      model.Add("Treats", treatList);
+      model.Add("Flavors", flavorList);
+      return View(model);
     }
-
   }
 }
