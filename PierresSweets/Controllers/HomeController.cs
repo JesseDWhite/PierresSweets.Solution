b72b1f9431d320935c40123b4d227535fd93812d
Jesse White <jesse.white6@gmail.com>
2021-06-04 16:11:43 -0700

    update to styling to buttons and main pages.

--- PierresSweets/Controllers/HomeController.cs
+++ PierresSweets/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ namespace PierresSweets.Controllers
 {
   public class HomeController : Controller
   {
+    private readonly PierresSweetsContext _db;
+
+    public HomeController(PierresSweetsContext db)
+    {
+      _db = db;
+    }
 
     [HttpGet("/")]
     public ActionResult Index()
