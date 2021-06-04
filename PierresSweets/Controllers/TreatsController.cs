5ebd4fdc4ef218bfaac3868e2e21017b7e735b11
Jesse White <jesse.white6@gmail.com>
2021-06-04 13:36:02 -0700

    create addfavorite method in controllers.

--- PierresSweets/Controllers/TreatsController.cs
+++ PierresSweets/Controllers/TreatsController.cs
@@ -34,12 +34,16 @@ namespace PierresSweets.Controllers
     {
       var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
       var currentUser = await _userManager.FindByIdAsync(userId);
-      var userTreats = _db.Treats.Where(entry => entry.User.Id == currentUser.Id).ToList();
+      var userTreats = _db.Treats
+      .Include(entry => entry.JoinEntities)
+      .Where(entry => entry.User.Id == currentUser.Id)
+      .ToList();
       return View(userTreats);
     }
 
     public ActionResult Create()
     {
+      ViewBag.User = new SelectList(_userManager.Users, "Id", "UserName");
       ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
       return View();
     }
@@ -90,6 +94,24 @@ namespace PierresSweets.Controllers
     }
 
     public ActionResult AddFlavor(int id)
+    {
+      var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+      ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
+      return View(thisTreat);
+    }
+
+    [HttpPost]
+    public ActionResult AddFavorite(Treat treat, int FlavorId)
+    {
+      if (FlavorId != 0)
+      {
+        _db.FlavorTreat.Add(new FlavorTreat() { FlavorId = FlavorId, TreatId = treat.TreatId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddFavorite(int id)
     {
       var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
       ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "FlavorName");
